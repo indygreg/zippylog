21c57eaa19b494dd1ee823517f976ba9e90e600e
Gregory Szorc <gregory.szorc@gmail.com>
2010-10-10 10:21:18 -0700

    clean up allocated memory in case of failure

--- src/zippylog/zeromq.cpp
+++ src/zippylog/zeromq.cpp
@@ -25,6 +25,10 @@ bool receive_multipart_message(socket_t * socket, vector<message_t *> &messages)
         message_t *msg = new message_t();
         if (!socket->recv(msg, 0)) {
             delete msg;
+            for (size_t i = 0; i < messages.size(); i++) {
+                delete messages[i];
+            }
+            messages.clear();
             return false;
         }
 
