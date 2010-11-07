b7b1e37d470b2776e292a3336b8a5ff105da6f42
Gregory Szorc <gregory.szorc@gmail.com>
2010-11-07 00:31:45 -0700

    add some time tests; change asserts to expects

--- src/zippylog/platform_unittest.cpp
+++ src/zippylog/platform_unittest.cpp
@@ -24,23 +24,64 @@ TEST(TimerTest, CreateTimers)
 
     Timer t(100000);
     ASSERT_NO_THROW(t.Start());
-    ASSERT_FALSE(t.Signaled());
+    EXPECT_FALSE(t.Signaled());
     sleep(105);
-    ASSERT_TRUE(t.Signaled());
+    EXPECT_TRUE(t.Signaled());
 
-    ASSERT_TRUE(t.Reset());
-    ASSERT_FALSE(t.Signaled());
+    EXPECT_TRUE(t.Reset());
+    EXPECT_FALSE(t.Signaled());
 
-    ASSERT_TRUE(t.Start(50000));
-    ASSERT_FALSE(t.Signaled());
+    EXPECT_TRUE(t.Start(50000));
+    EXPECT_FALSE(t.Signaled());
     sleep(51);
-    ASSERT_TRUE(t.Signaled());
+    EXPECT_TRUE(t.Signaled());
 
-    ASSERT_TRUE(t.Start(100000));
-    ASSERT_TRUE(t.Reset());
-    ASSERT_FALSE(t.Signaled());
+    EXPECT_TRUE(t.Start(100000));
+    EXPECT_TRUE(t.Reset());
+    EXPECT_FALSE(t.Signaled());
     sleep(105);
-    ASSERT_FALSE(t.Signaled());
+    EXPECT_FALSE(t.Signaled());
 }
 
+TEST(TimeTest, TimeConversion)
+{
+    Time t;
+    ASSERT_TRUE(UnixMicroTimeToZippyTime(0, t));
+    EXPECT_EQ(1970, t.year);
+    EXPECT_EQ(1, t.mon);
+    EXPECT_EQ(1, t.mday);
+    EXPECT_EQ(0, t.epoch_sec);
+    EXPECT_EQ(0, t.epoch_micro);
+    EXPECT_EQ(0, t.usec);
+    EXPECT_EQ(0, t.hour);
+    EXPECT_EQ(0, t.min);
+    EXPECT_EQ(0, t.sec);
+    EXPECT_EQ(0, t.yday);
+    EXPECT_EQ(4, t.wday);
 
+    ASSERT_TRUE(UnixMicroTimeToZippyTime(1234567890000000, t));
+    EXPECT_EQ(2009, t.year);
+    EXPECT_EQ(2, t.mon);
+    EXPECT_EQ(13, t.mday);
+    EXPECT_EQ(1234567890, t.epoch_sec);
+    EXPECT_EQ(1234567890000000, t.epoch_micro);
+    EXPECT_EQ(0, t.usec);
+    EXPECT_EQ(23, t.hour);
+    EXPECT_EQ(31, t.min);
+    EXPECT_EQ(30, t.sec);
+    EXPECT_EQ(43, t.yday);
+    EXPECT_EQ(5, t.wday);
+
+    ASSERT_TRUE(UnixMicroTimeToZippyTime(1234567890001024, t));
+    EXPECT_EQ(2009, t.year);
+    EXPECT_EQ(2, t.mon);
+    EXPECT_EQ(13, t.mday);
+    EXPECT_EQ(1234567890, t.epoch_sec);
+    EXPECT_EQ(1234567890001024, t.epoch_micro);
+    EXPECT_EQ(1024, t.usec);
+    EXPECT_EQ(23, t.hour);
+    EXPECT_EQ(31, t.min);
+    EXPECT_EQ(30, t.sec);
+    EXPECT_EQ(43, t.yday);
+    EXPECT_EQ(5, t.wday);
+}
