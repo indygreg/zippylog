a2c731d73b507c781acaa3673c9bd933b97c1860
Gregory Szorc <gregory.szorc@gmail.com>
2011-03-26 20:05:52 -0700

    add autogenerated Lua bindings for protocol buffer messages

---
+++ src/lua-protobuf.cc
@@ -0,0 +1,29 @@
+
+
+#include "lua-protobuf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <lauxlib.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+int lua_protobuf_enum_index(lua_State *L)
+{
+    return luaL_error(L, "attempting to access undefined enumeration value: %s", lua_tostring(L, 2));
+}
+
+int lua_protobuf_enum_newindex(lua_State *L)
+{
+    return luaL_error(L, "cannot modify enumeration tables");
+}
+
+int lua_protobuf_gc_always_free(::google::protobuf::Message *msg, void *ud)
+{
+    return 1;
+}
+
---
+++ src/lua-protobuf.h
@@ -0,0 +1,37 @@
+
+#ifndef LUA_PROTOBUF_H
+#define LUA_PROTOBUF_H
+
+#include <google/protobuf/message.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <lua.h>
+
+#ifdef WINDOWS
+#define LUA_PROTOBUF_EXPORT __declspec(stdcall)
+#else
+#define LUA_PROTOBUF_EXPORT
+#endif
+
+// type for callback function that is executed before Lua performs garbage
+// collection on a message instance.
+// if called function returns 1, Lua will free the memory backing the object
+// if returns 0, Lua will not free the memory
+typedef int (*lua_protobuf_gc_callback)(::google::protobuf::Message *msg, void *userdata);
+
+// __index and __newindex functions for enum tables
+LUA_PROTOBUF_EXPORT int lua_protobuf_enum_index(lua_State *L);
+LUA_PROTOBUF_EXPORT int lua_protobuf_enum_newindex(lua_State *L);
+
+// GC callback function that always returns true
+LUA_PROTOBUF_EXPORT int lua_protobuf_gc_always_free(::google::protobuf::Message *msg, void *userdata);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
