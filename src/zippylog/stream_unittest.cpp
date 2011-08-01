67e9a7b8cc564ac5f0d756c3196cebf95843edf5
Gregory Szorc <gregory.szorc@gmail.com>
2011-07-31 17:23:03 -0700

    low-level tests for stream writing

--- src/zippylog/stream_unittest.cpp
+++ src/zippylog/stream_unittest.cpp
@@ -14,6 +14,7 @@
 
 #include <zippylog/testing.hpp>
 
+#include <zippylog/platform.hpp>
 #include <zippylog/stream.hpp>
 #include <zippylog/envelope.hpp>
 
@@ -22,11 +23,75 @@
 using ::std::string;
 using ::zippylog::Envelope;
 using ::zippylog::FileInputStream;
+using ::zippylog::FileOutputStream;
+
+namespace zippylog {
 
 class StreamTest : public ::zippylog::testing::TestBase
 { };
 
 string path = "test/stores/01-singlestream/A/B/2010-11-26-07.zippylog";
+
+TEST_F(StreamTest, FileOutputStreamConstructor)
+{
+    string dir = this->GetTemporaryDirectory();
+    string path = platform::PathJoin(dir, "s1.zippylog");
+
+    ASSERT_NO_THROW(FileOutputStream fos(path, false)) << "create FileOutputStream without lock";
+    ASSERT_NO_THROW(FileOutputStream fos(path, true)) << "create FileOutputStream with lock";
+}
+
+TEST_F(StreamTest, FileOutputStreamStreamFormat)
+{
+    string path = platform::PathJoin(this->GetTemporaryDirectory(), "s1.zippylog");
+
+    Envelope e1, e2;
+    e1.AddTag("foo");
+    e1.AddTag("bar");
+    e1.SetStringValueField("string value");
+    e2.SetStringValueField("another envelope");
+
+    ASSERT_EQ(33, e1.SerializedByteSize());
+    ASSERT_EQ(27, e2.SerializedByteSize());
+
+    // in a closure so the output stream gets destroyed
+    {
+        FileOutputStream fos(path, true);
+        ASSERT_TRUE(fos.WriteEnvelope(e1));
+        ASSERT_TRUE(fos.WriteEnvelope(e2));
+    }
+
+    platform::FileStat fs;
+    ASSERT_TRUE(platform::stat(path, fs)) << "able to stat written stream";
+
+    // stream header + envelope length header + envelope data
+    EXPECT_EQ(1 + 2 + e1.SerializedByteSize() + e2.SerializedByteSize(), fs.size)
+        << "stream is correct size";
+
+    platform::File f;
+    ASSERT_TRUE(f.Open(path, platform::File::READ | platform::File::BINARY));
+
+    int fd = f.FileDescriptor();
+    ASSERT_GT(fd, 0) << "file descriptor for reading is valid";
+
+    char *data = new char[fs.size];
+    EXPECT_EQ(fs.size, read(fd, data, fs.size));
+
+    EXPECT_EQ(0x01, data[0]);
+    EXPECT_EQ(33, data[1]);
+    EXPECT_EQ(27, data[1 + 1 + 33]);
+
+    EXPECT_NO_THROW(Envelope e1p((void *)(data + 2), 33));
+    EXPECT_NO_THROW(Envelope e2p((void *)(data + 1 + 1 + 33 + 1), 27));
+
+    Envelope e1p((void *)(data + 2), 33);
+    Envelope e2p((void *)(data + 2+33+1), 27);
+
+    EXPECT_EQ(e1, e1p);
+    EXPECT_EQ(e2, e2p);
+
+    delete [] data;
+}
 
 TEST_F(StreamTest, FileInputStreamConstructor)
 {
@@ -78,3 +143,5 @@ TEST_F(StreamTest, FileInputStreamReading)
         EXPECT_EQ(44 + 12 + 1, f.CurrentEnvelopeOffset());
     }
 }
+
+} // namespace
