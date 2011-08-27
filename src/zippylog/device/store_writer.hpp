253d85e496968b96a1eb8bffe6e8d539ad59abef
Gregory Szorc <gregory.szorc@gmail.com>
2011-08-27 16:28:14 -0700

    documentation

--- src/zippylog/device/store_writer.hpp
+++ src/zippylog/device/store_writer.hpp
@@ -29,6 +29,7 @@ namespace device {
 /// Class parameters for the store writer device
 class ZIPPYLOG_EXPORT StoreWriterStartParams {
 public:
+    /// Construct a new parameters set
     StoreWriterStartParams() :
       ctx(NULL),
       active(NULL)
@@ -102,35 +103,64 @@ public:
     ::zippylog::device::PumpResult Pump(int32 timeout = 0);
 
 protected:
+    /// 0MQ context to use
     ::zmq::context_t *ctx;
 
-    // whether we own the 0MQ context
+    /// Whether we own the 0MQ context
     bool own_context;
 
+    /// The path of the store to bind to
     ::std::string store_path;
+
+    /// The store instance we are writing to
     ::zippylog::Store *store;
 
+    /// 0MQ endpoint for socket for writes that don't require responses
     ::std::string envelope_pull_endpoint;
+
+    /// 0MQ endpoint for socket for writes that require responses
     ::std::string envelope_rep_endpoint;
 
+    /// 0MQ PULL socket receiving envelopes that write without responses
     ::zmq::socket_t *envelope_pull_sock;
+
+    /// 0MQ REP socket receiving envelopes that write with responses
     ::zmq::socket_t *envelope_rep_sock;
 
+    /// 0MQ structure used by poll
     ::zmq::pollitem_t *pollitem;
 
+    /// Defines the result of a ReceiveAndWrite() call
     enum ReceiveResult {
+        /// Operation completed successfully
         OK = 1,
+
+        /// No messages were seen on the socket
         NO_MESSAGES = 2,
+
+        /// There was an error receiving messages
         RECEIVE_ERROR = 3,
     };
 
+    /// Index of pull socket in pollitem struct
     int envelope_pull_index;
+
+    /// Index of rep socket in pollitem struct
     int envelope_rep_index;
+
+    /// The number of active sockets in the poll structure
     int active_sockets;
 
+    /// Process a ready message on the pull socket
     bool ProcessEnvelopePull();
+
+    /// Process a ready message on the rep socket
     bool ProcessEnvelopeRep();
 
+    /// Receive a message from a socket and write it
+    ///
+    /// @param sock The socket to receive the message from
+    /// @return The result of the operation
     ReceiveResult ReceiveAndWrite(::zmq::socket_t *sock);
 
 private:
