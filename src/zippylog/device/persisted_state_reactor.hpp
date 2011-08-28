38eaa33310a8a3351abe952057f27f65b885e239
Gregory Szorc <gregory.szorc@gmail.com>
2011-08-27 20:26:02 -0700

    documentation

--- src/zippylog/device/persisted_state_reactor.hpp
+++ src/zippylog/device/persisted_state_reactor.hpp
@@ -27,8 +27,10 @@
 namespace zippylog {
 namespace device {
 
+/// Container for parameters used to initialize PersistedStateReactor
 class ZIPPYLOG_EXPORT PersistedStateReactorStartParams {
 public:
+    /// Create a new container
     PersistedStateReactorStartParams() :
         ctx(NULL),
         active(NULL)
@@ -37,10 +39,19 @@ public:
     /// 0MQ context
     ::zmq::context_t *ctx;
 
+    /// Endpoint for socket that should receive store changes
     ::std::string store_change_endpoint;
+
+    /// Endpoint for socket that leads back to clients
     ::std::string client_endpoint;
+
+    /// Endpoint for socket that receives new persisted state requests
     ::std::string subscription_endpoint;
+
+    /// Endpoint for socket that receives updates from clients
     ::std::string subscription_updates_endpoint;
+
+    /// Endpoint for a logger to send logging information
     ::std::string logger_endpoint;
 
     /// Semaphore stating whether device should remain active
@@ -77,9 +88,16 @@ public:
     ::zippylog::device::PumpResult Pump(int32 timeout_microseconds);
 protected:
 
+    ///@{
+    /// Persisted state manager handlers
+    ///
+    /// The persisted state manager calls into these for every subscription
+    /// that cares about them. The functions formulate a response and send
+    /// them to the client.
     static void PathAddedCallback(SubscriptionInfo const &subscription, ::std::string const &path, void *data);
     static void PathDeletedCallback(SubscriptionInfo const &subscription, ::std::string const &path, void *data);
     static void StreamAppendedCallback(SubscriptionInfo const &subscription, EnvelopeSubscriptionResponseState &state, void *data);
+    ///@}
 
     /// Initializes the object
     ///
@@ -89,30 +107,52 @@ protected:
     /// Processes a received store change message
     void ProcessStoreChangeMessage(::zmq::message_t &msg);
 
+    /// Process a subscription update message
     void ProcessSubscriptionUpdate(::zmq::message_t &msg);
 
-    // variables passed in to constructor
+    /// 0MQ context
     ::zmq::context_t *ctx;
 
+    /// Endpoint on which we listen for store change notifications
     ::std::string store_changes_endpoint;
+
+    /// Endpoint that leads back to clients
     ::std::string client_endpoint;
+
+    /// Endpoint that receives new persisted state requests
     ::std::string subscription_endpoint;
+
+    /// Endpoint that receives persisted state updates
     ::std::string subscription_updates_endpoint;
+
+    /// Endpoint for where to send log messages
     ::std::string logger_endpoint;
 
+    /// Parameters used to initialize the PersistedStateManager
     PersistedStateManagerStartParams manager_params;
 
-    // created by instance
+    /// Identifier for this instance
     ::std::string id;
 
+    /// SUB that receives store change notifications
     ::zmq::socket_t *store_changes_sock;
+
+    /// PUSH that sends messages back to clients
     ::zmq::socket_t *client_sock;
+
+    /// PULL that receives new persisted state requests
     ::zmq::socket_t *subscription_sock;
+
+    /// SUB that receives persisted state updates
     ::zmq::socket_t *subscription_updates_sock;
+
+    /// PUSH that sends logging messages
     ::zmq::socket_t *logger_sock;
 
+    /// Polling structure for 0MQ
     ::zmq::pollitem_t pollitems[3];
 
+    /// The thing that actually handles persisted state
     PersistedStateManager *manager;
 
 private:
